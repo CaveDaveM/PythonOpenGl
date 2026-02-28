6df0086addb0e1c21ae1940c9d5b232d708714a7
CaveDaveM <149399428+CaveDaveM@users.noreply.github.com>
2026-02-28 15:18:23 +0000

    Chapter One, Chapter Two

---
+++ Chapter_One/TestPython.py
@@ -0,0 +1,23 @@
+﻿import pygame
+pygame.init()
+ScreenWidth = 1000
+ScreenHeight = 800
+screen = pygame.display.set_mode((ScreenWidth, ScreenHeight))
+
+done = False
+white = pygame.Color(255, 255, 255)
+
+def to_pygame_coordinates(display, x, y):
+    return x, display.get_height() - y
+
+def draw_rect(x,y,size):
+    pygame.draw.rect(screen, white, (x, y, size, size))
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    draw_rect(345,345,20)
+    pygame.display.update()
+pygame.quit()
---
+++ Chatper_Two/RGBspace.py
@@ -0,0 +1,19 @@
+﻿import pygame
+
+pygame.init()
+ScreenWidth = 1000
+ScreenHeight = 800
+Screen = pygame.display.set_mode((ScreenWidth, ScreenHeight))
+
+done = False
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    for y in range(ScreenHeight):
+        for x in range(ScreenWidth):
+            Screen.set_at((x, y), pygame.Color(0,
+                                               int(y / ScreenHeight * 255), 255))
+    pygame.display.update()
+pygame.quit()
