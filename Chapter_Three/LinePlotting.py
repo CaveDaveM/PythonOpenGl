560f284bddfefb4685162561c3aea22507b753cb
CaveDaveM <149399428+CaveDaveM@users.noreply.github.com>
2026-03-01 21:28:32 +0000

    Bresenham algorithm

---
+++ Chapter_Three/LinePlotting.py
@@ -0,0 +1,29 @@
+﻿import pygame
+pygame.init()
+
+
+Screen = pygame.display.set_mode((1000, 800))
+
+done = False
+white = pygame.Color(255, 255, 255)
+green = pygame.Color(0, 255, 0)
+XOriginOffSet = int(Screen.get_width()/2)
+YOriginOffSet = int(Screen.get_height()/2)
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    #this is for the x axis
+    for x in range(-500,500):
+        Screen.set_at((x + XOriginOffSet,YOriginOffSet),green)
+
+    for y in range(-400,400):
+        Screen.set_at((XOriginOffSet,y+YOriginOffSet),green)
+
+    for x in range(-500,500):
+        y = 10 * x - 100
+        Screen.set_at((x + XOriginOffSet, y + YOriginOffSet),white)
+
+    pygame.display.update()
+pygame.quit()
---
+++ Chapter_Three/pylinenative.py
@@ -0,0 +1,44 @@
+﻿import pygame
+from pygame.locals import *
+
+pygame.init()
+Screen = pygame.display.set_mode((1000,800))
+
+
+done = False
+white = pygame.Color(255,255,255)
+green = pygame.Color(0,255,0)
+TimesClicked = 0
+
+def PlotLineAlgorithm(p1, p2):
+    if p2[0] < p1[0]:
+        temp = p2
+        p2 = p1
+        p1 = temp
+    x0 , y0 = p1
+    x1 , y1 = p2
+    magnitude = (y1 - y0)/ (x1 - x0)
+    yIntercept = y0 - (magnitude * x0)
+
+    
+    for x in range(x0, x1):
+        y = magnitude * x + yIntercept
+        Screen.set_at((int(x), int(y)), white)
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        elif event.type == MOUSEBUTTONDOWN:
+            if TimesClicked == 0:
+                point1 = pygame.mouse.get_pos()
+            else:
+                point2 = pygame.mouse.get_pos()
+            TimesClicked += 1
+            if TimesClicked > 1:
+                PlotLineAlgorithm(point1, point2)
+                TimesClicked = 0
+    pygame.display.update()
+pygame.quit()
+
+
