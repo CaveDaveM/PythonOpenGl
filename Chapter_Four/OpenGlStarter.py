a617b523083edf35bdd523ae852b0bdcab5921a8
CaveDaveM <149399428+CaveDaveM@users.noreply.github.com>
2026-03-02 20:15:15 +0000

    Chapter five cube render

--- Chapter_Four/OpenGlStarter.py
+++ Chapter_Four/OpenGlStarter.py
@@ -1,19 +1,27 @@
 ﻿import pygame
 from PyMesh import *
 from pygame.locals import *
-from OpenGL.GL import *
+from OpenGL.GLU import *
+
 pygame.init()
-Screen = pygame.display.set_mode((500, 500),DOUBLEBUF|OPENGL)
+ScreenWidth = 500
+ScreenHeight = 500
+Screen = pygame.display.set_mode((ScreenWidth, ScreenHeight),DOUBLEBUF|OPENGL)
 
 pygame.display.set_caption("OpenGL")
 done = False
-mesh = Mesh3D()
+white = pygame.Color(255,255,255)
+gluPerspective(60, (ScreenWidth/ ScreenHeight), 0.1, 100)
+glTranslatef(0.0,0.0,-3)
+mesh = Cube()
 while not done:
     #the for loop is for click events such as button presses
     for event in pygame.event.get():
         if event.type == QUIT:
             done = True
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    glRotatef(5, 1, 0, 1)
     mesh.draw()
     pygame.display.flip()
+    pygame.time.wait(100)
 pygame.quit()
--- Chapter_Four/PyMesh.py
+++ Chapter_Four/PyMesh.py
@@ -15,3 +15,28 @@ class Mesh3D:
             glVertex3fv(self.vertices[self.triangles[t+1]])
             glVertex3fv(self.vertices[self.triangles[t+2]])
             glEnd()
+
+class Cube:
+    def __init__(self):
+        self.vertices = [(0.5, -0.5, 0.5),
+                        (-0.5, -0.5, 0.5), (0.5, 0.5, 0.5),
+                        (-0.5, 0.5, 0.5),(0.5, 0.5, -0.5),
+                        (-0.5, 0.5, -0.5), (0.5, -0.5, -0.5), (-0.5, -0.5, -0.5),
+                        (0.5, 0.5, 0.5), (-0.5, 0.5, 0.5), (0.5, 0.5, -0.5),
+                        (-0.5, 0.5, -0.5), (0.5, -0.5, -0.5), (0.5, -0.5, 0.5),
+                        (-0.5, -0.5, 0.5), (-0.5, -0.5, -0.5), (-0.5, -0.5, 0.5),
+                        (-0.5, 0.5, 0.5), (-0.5, 0.5, -0.5), (-0.5, -0.5, -0.5),
+                        (0.5, -0.5, -0.5), (0.5, 0.5, -0.5), (0.5, 0.5, 0.5),
+                        (0.5, -0.5, 0.5)]
+        #who types out all this, this is a joke
+        self.triangles = [0, 2, 3, 0, 3, 1, 8, 4, 5, 8, 5, 9, 10, 6,7, 10,
+                        7, 11, 12, 13, 14, 12, 14, 15, 16, 17, 18, 16,
+                        18, 19, 20, 21, 22, 20, 22, 23]
+
+    def draw(self):
+        for t in range(0, len(self.triangles), 3):
+            glBegin(GL_LINE_LOOP)
+            glVertex3fv(self.vertices[self.triangles[t]])
+            glVertex3fv(self.vertices[self.triangles[t + 1]])
+            glVertex3fv(self.vertices[self.triangles[t + 2]])
+            glEnd()
